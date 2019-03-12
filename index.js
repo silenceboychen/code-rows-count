9023f52510573468f281db9f46ca08b63c19ae42
silenceboychen <302835677@qq.com>
2019-03-12 17:00:45 +0800

    feat: 统计指定目录或者文件里的代码行数

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
 const fs = require('fs');
+const path = require('path');
 const _ = require('lodash');
-let arr;
-fs.readFile('README.md', function (res, data) {
-    arr = data.toString().split('\n');
-    console.log(arr);
-    _.remove(arr, (item) => {
-        if (item === '') {
-            return true;
+let allCount = 0;
+let fileCount = 0;
+const filePath = path.resolve(__dirname, '../../chenhao/test');
+function readFile (filePath, callback) {
+    fs.stat(filePath, (error, stats) => {
+        if (error) {
+            console.log(filePath);
+            console.error('获取文件stats失败');
+            console.log(error);
+        } else {
+            const isFile = stats.isFile();
+            const isDir = stats.isDirectory();
+            if (isDir) {
+                readDir(filePath, callback);
+            }
+            if (isFile) {
+                fs.readFile(filePath, 'utf8', (err, data) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    const arr = data.toString().split('\n');
+                    _.remove(arr, (item) => {
+                        if (item === '') {
+                            return true;
+                        }
+                    });
+                    console.log('文件路径:' + filePath + ', 文件行数:' + arr.length);
+                    allCount += arr.length;
+                    fileCount++;
+                    callback();
+                });
+            }
         }
     });
-    console.log(arr.length);
+}
+
+function readDir (filePath, callback) {
+    fs.readdir(filePath, (err, files) => {
+        if (err) {
+            console.error(err);
+        } else {
+            let count = 0;
+            const checkEnd = () => {
+                ++count === files.length && callback();
+            };
+            for (let i = 0; i < files.length; i++) {
+                if (files[i] === 'node_modules') {
+                    checkEnd();
+                    continue;
+                }
+                const fp = path.join(filePath, files[i]);
+                readFile(fp, checkEnd);
+            }
+            // 为空时直接回调
+            files.length === 0 && callback();
+        }
+    });
+}
+const timeStart = new Date();
+readFile(filePath, function () {
+    console.log('done', new Date() - timeStart);
+    console.log('总文件数:' + fileCount + ', 总代码行数: ' + allCount);
 });
